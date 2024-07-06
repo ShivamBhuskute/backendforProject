b2a1b5ca962244754d46a4742d75f352630c90b6
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-07-06 18:30:36 +0530

    routes

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,18 +1,22 @@
-// for any error that may accur also we have 
-// inherited from "Error" which is inbuilt
-
 class ApiError extends Error {
     constructor(
         statusCode,
         message = "Something went wrong",
         errors = [],
-        stack = "") {
+        stack = ""
+    ) {
         super(message)
-        this.statusCode = statusCode,
-            this.data = null,
-            this.message = message,
-            this.success = false,
-            this.errors = errors
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = errors
+
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
 
     }
 }
