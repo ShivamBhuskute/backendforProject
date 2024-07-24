171e79cc975c098a106f4cdffddd91e5b3d18541
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-07-24 20:54:38 +0530

    debugged

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,17 +6,17 @@ import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
 
 router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1,
+        },
+        {
+            name: "coverImage",
+            maxCount: 1,
+        },
+    ]),
     registerUser
-    // upload.fields([
-    //     {
-    //         name: "avatar",
-    //         maxCount: 1
-    //     },
-    //     {
-    //         name: "coverImage",
-    //         maxCount: 1
-    //     }
-    // ]),
 );
 
 export default router;
